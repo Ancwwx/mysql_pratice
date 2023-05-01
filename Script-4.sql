67818930ef230048b9ca4aab5adc763c62e1090c
Ancwwx <ancwwx@gmail.com>
2023-05-01 23:33:11 +0900

    mhl database

--- Script-4.sql
+++ Script-4.sql
@@ -7,3 +7,23 @@ CREATE TABLE actor (
 SELECT * FROM actor;    
 INSERT INTO actor VALUES(NULL,'燕小六','男','1970-12-12','110');
 INSERT INTO actor VALUES(NULL,'jack','男','1999-11-11','112');
+
+
+create table account(
+                   id int primary key auto_increment, 
+                   name varchar(32) not null default '',
+                   balance double not null default 0) 
+                   character set utf8;
+insert into account values(null, '马云', 3000); 
+insert into account values(null, '马化腾',10000);
+
+SELECT * FROM account;
+
+
+CREATE TABLE admin2
+                 (id INT PRIMARY KEY AUTO_INCREMENT,
+                  username VARCHAR (32) NOT NULL,
+                  password VARCHAR (32) NOT NULL) ;
+                 
+DROP TABLE admin2;            
+ SELECT * FROM admin2;              
